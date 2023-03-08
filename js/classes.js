83aaa66aedc376f32b7a33762c388d1bd8c10bd2
Micro08 <pedrogelisson@gmail.com>
2023-03-07 22:14:51 -0300

    parte 2

---
+++ js/classes.js
@@ -0,0 +1,28 @@
+class Ator{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+
+class Diretor{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+class Filme{
+    constructor(id,titulo,ano,genero,duracao,sinopse,cartaz,direcao,elenco,classificacao,avaliacao){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.genero=genero;
+        this.duracao=duracao;
+        this.sinopse=sinopse;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.elenco=elenco;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+    }
+}
